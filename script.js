9b0164e46afd1179d01b58c3409e9a1bcefe5716
MaharshiJ88 <136585286+MaharshiJ88@users.noreply.github.com>
2023-12-08 21:43:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function firstWord(s) {
+function firstWord(str) {
   // your code here
+	// Trim leading and trailing whitespaces
+  const trimmedStr = str.trim();
+
+  // Check if the trimmed string is empty
+  if (trimmedStr === '') {
+    return '';
+  }
+
+  // Split the string by space and return the first element
+  const words = trimmedStr.split(' ');
+  return words[0];
 }
 
 // Do not change the code below
